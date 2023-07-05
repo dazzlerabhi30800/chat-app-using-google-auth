9bcfb8c0ddd94e5e9610b17e3321c98110e5b112
= <abhisingh30800@gmail.com>
2023-07-05 10:15:35 +0530

    initial commit in windows

--- src/Firebase-Config.jsx
+++ src/Firebase-Config.jsx
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "@firebase/firestore";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
@@ -16,3 +17,22 @@ const auth = getAuth(app);
 const provider = new GoogleAuthProvider();
 
 export { db, auth, provider };
+=======
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAa9rlPtYU2id6zsLO5BX2uwLiwan8XRUE",
+  authDomain: "chat-app-6eb15.firebaseapp.com",
+  projectId: "chat-app-6eb15",
+  storageBucket: "chat-app-6eb15.appspot.com",
+  messagingSenderId: "385979646779",
+  appId: "1:385979646779:web:65ea615de7af1c95cd2d1e",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+>>>>>>> 4e448c5 (initial commit in windows)
